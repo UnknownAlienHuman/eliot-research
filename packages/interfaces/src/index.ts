9a8ef7b8c0f8efbb8b323407ba1a24c4a5a062f8
Iurii Lisenkov <71514792+UnknownAlienHuman@users.noreply.github.com>
2026-08-31 13:31:59 -0400

    refactor(er21): retire duplicate ingest API surface

--- packages/interfaces/src/index.ts
+++ packages/interfaces/src/index.ts
@@ -3,5 +3,4 @@ export * from "./semantic-api.js";
 export * from "./owner-api.js";
 export * from "./routes.js";
 export * from "./application.js";
-export * from "./ingest-api.js";
 export * from "./federation-api.js";
