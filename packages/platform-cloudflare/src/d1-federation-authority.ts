3a9bbc7673ebcbeea7d66d577b087436bc9917b6
Iurii Lisenkov <71514792+UnknownAlienHuman@users.noreply.github.com>
2026-09-04 11:20:07 -0400

    ER-13: move federation runtime to Worker-owned packet

--- packages/platform-cloudflare/src/d1-federation-authority.ts
+++
@@ -1,3 +0,0 @@
-export * from "./d1-federation-types.js";
-export { createD1FederationReferenceManifestAuthority } from "./d1-federation-manifest.js";
-export { createD1FederationJobAuthority } from "./d1-federation-job.js";
